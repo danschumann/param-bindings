bb6fa01946abcd6287a6e18125268470ee9e951a
danschumann <dan.schumann@gmail.com>
2014-06-16 15:59:19 -0500

    0.0.3 made work with ?

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ var
   __slice   = [].slice;
 
 
-wrap = function(connection, executeName) {
+wrap = function(connection, executeName, options) {
   var execute;
   execute = connection[executeName];
 
+  options = options || {};
+  options.key = options.key || '?';
+  if (options.increment && options.startAt == null)
+    throw new Error('param-bindings:  To specify .increment, you must also specify .startAt (usually 1)');
+
   return connection[executeName] = function(sql, args) {
 
-    if (_.isArray(args)) {
+    if (_.isArray(args) || _.isFunction(args) || !_.isObject(args)) {
       return execute.apply(connection, arguments);
     }
 
@@ -23,11 +28,24 @@ wrap = function(connection, executeName) {
     debug('paramNames', paramNames);
     debug('before conversion', sql, args);
 
-    _.each(paramNames, function(pn, n) {
-      pn = pn.substring(1); // strip the : from the string
-      sql = sql.replace(pn, (n + 1)); // replace :ham with :1
-      newArgs.push(args[pn]);
-    });
+    // if we zipper paramNames and pieces, they would equal the original
+    var pieces = sql.split(findArgs);
+    debug('pieces', pieces);
+
+    compiled = [pieces.shift()];
+
+    var pName;
+    var n = options.startAt;
+
+    while(pieces.length) {
+      pName = paramNames.shift();
+      compiled.push(options.increment ? ':' + n++ : '?');
+      compiled.push(pieces.shift());
+
+      newArgs.push(args[pName.substring(1)]);
+    }
+
+    sql = compiled.join('');
 
     debug('after conversion', sql, newArgs);
 
@@ -51,6 +69,21 @@ if (process.env.TEST_PARAM_BINDINGS) {
 
 
   wrap( fakeConnection, 'fakeExecute' )
+
+  debug('TEST question mark');
+
+  fakeConnection.fakeExecute(
+    'select * from users ' + 
+      'join bacon on bacon.id = :ham, :column=:yummy ' + 
+      'where beep=:boop and bop=:blip',
+    {ham: 'pork', yummy: 10, boop: 2, blip: 'bop', column: 'fun'},
+    function(){
+      console.log('called back');
+  });
+
+  wrap( fakeConnection, 'fakeExecute', {increment: true, startAt: 1})
+
+  debug('TEST increment');
 
   fakeConnection.fakeExecute(
     'select * from users ' + 
